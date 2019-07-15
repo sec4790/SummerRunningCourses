590f6bf940b02cb550b1d1ed26dd3fd77dc92e7a
Jmckamie <52890836+Jmckamie@users.noreply.github.com>
2019-07-15 16:22:11 -0500

    Add files via upload

---
+++ SchedulingSystem/src/summer2019CPSC4360/application/Instructor.java
@@ -0,0 +1,8 @@
+package summer2019CPSC4360.application;
+
+public class Instructor {
+	private String name;
+	private String username;
+	private String password;
+	
+}
---
+++ SchedulingSystem/src/summer2019CPSC4360/userInterface/loginGUI.java
@@ -0,0 +1,23 @@
+package summer2019CPSC4360.userInterface;
+
+import java.awt.*;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class loginGUI extends JDialog{
+	
+	private JLabel loginUsernameLabel;
+	private JLabel loginPasswordLabel;
+	private JTextField loginUsernameField;
+	private JPasswordField loginPasswordField;
+	
+	public loginGUI(Frame parent) {
+		super(parent, "Scheduling System: Login", true);
+		JPanel loginPanel = new JPanel();
+		
+	}
+
+}
